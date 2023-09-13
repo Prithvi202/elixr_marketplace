27a674c75cc3285e16597cd6650c1662de110ea3
Prithvi M Potti <imailprithvi@gmail.com>
2023-09-13 14:48:59 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/elixr_marketplace/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.elixr_marketplace
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/marketplace_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'marketplace_event.dart';
+part 'marketplace_state.dart';
+
+class MarketplaceBloc extends Bloc<MarketplaceEvent, MarketplaceState> {
+  MarketplaceBloc() : super(MarketplaceInitial()) {
+    on<MarketplaceEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
